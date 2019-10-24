698199869d743dff517ba121b05ef1dd35572c86
Vladislav Eroshenko <vladislav400@gmail.com>
2019-10-24 12:35:17 +0300

    Refactored serial_ports, added listen_port

--- monitor.py
+++ monitor.py
@@ -3,9 +3,10 @@ import serial
 import sys
 import time
 
+dict_of_ports = {}
+
 
 def serial_ports():
-    start_time = time.time()
     """ Lists serial port names
 
         :raises EnvironmentError:
@@ -13,6 +14,7 @@ def serial_ports():
         :returns:
             A list of the serial ports available on the system
     """
+    start_time = time.time()
 
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -24,23 +26,53 @@ def serial_ports():
     else:
         raise EnvironmentError('Unsupported platform')
 
-    print("--- Checked system %.5f seconds ---" % (time.time() - start_time))
+    print("--- Checked system in %.5f seconds ---"
+          % (time.time() - start_time))
 
     result = []
+    port_counter = 1
     for port in ports:
         start_check_time = time.time()
         try:
-            s = serial.Serial(port)
-            s.close()
+            ser = serial.Serial(port)
+            ser.close()
             result.append(port)
+            dict_of_ports[port_counter] = port
+            port_counter += 1
         except (OSError, serial.SerialException):
             pass
-        print("--- Checked port %s %.1f seconds ---" %
+        print("--- Checked port %s in %.5f seconds ---" %
               (str(port), (time.time() - start_check_time)))
     return result
+
+
+def listen_port():
+    """ Listens to a specified port
+    """
+    print("Available ports:")
+    print(dict_of_ports)
+    try:
+        port_number = int(input("Choose the port's number: "))
+    except ValueError:
+        print("Wrong input. Enter integer.")
+        return
+
+    start_time = time.time()
+
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.parity = serial.PARITY_EVEN
+    ser.port = dict_of_ports[port_number]
+    ser.open()
+    ser.close()
+
+    print("--- Listening time %.5f seconds ---"
+          % (time.time() - start_time))
 
 
 if __name__ == '__main__':
     start_time = time.time()
     print(serial_ports())
-    print("--- Total time %.5f seconds ---" % (time.time() - start_time))
+    print("--- Total checking time %.5f seconds ---"
+          % (time.time() - start_time))
+    listen_port()
