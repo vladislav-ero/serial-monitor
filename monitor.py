fc5126f365885b268de3421e2f62ca3560ec1308
Vladislav Eroshenko <vladislav400@gmail.com>
2019-10-24 17:27:57 +0300

    Added combining algorithm to listen_port function

--- monitor.py
+++ monitor.py
@@ -1,7 +1,8 @@
 import glob
-import serial
 import sys
 import time
+
+import serial
 
 dict_of_ports = {}
 
@@ -46,28 +47,50 @@ def serial_ports():
     return result
 
 
-def listen_port():
-    """ Listens to a specified port
+def listen_port(array_size=50):
+    """
+        Listens to a specified port
+        Recieves 3 packets of 4 bits
+        Combines them together into single 12 bit digit
     """
     print("Available ports:")
     print(dict_of_ports)
-    try:
-        port_number = int(input("Choose the port's number: "))
-    except ValueError:
-        print("Wrong input. Enter integer.")
-        return
+    # try:
+    #     port_number = int(input("Choose the port's number: "))
+    # except ValueError:
+    #     print("Wrong input. Enter integer.")
+    #     return
 
-    start_time = time.time()
+    listen_port_start_time = time.time()
 
     ser = serial.Serial()
     ser.baudrate = 115200
     ser.parity = serial.PARITY_EVEN
-    ser.port = dict_of_ports[port_number]
+    # ser.port = dict_of_ports[port_number]
+    ser.port = '/dev/tty.SLAB_USBtoUART'
     ser.open()
+    read_bytes = ser.read(3 * array_size)
     ser.close()
 
     print("--- Listening time %.5f seconds ---"
+          % (time.time() - listen_port_start_time))
+
+    print(read_bytes)
+
+    start_time = time.time()
+    adc_results = [0] * array_size
+    byte_counter = 0
+    for byte in read_bytes:
+        print('0x%x' % int(bin(byte), 2))
+        packet_position = 2 - (byte_counter - ((byte_counter // 3) * 3))
+        packet = (int(bin(byte), 2) & 0xF) << (packet_position * 4)
+        adc_results[byte_counter // 3] += packet
+        byte_counter += 1
+    # print(bin(ord(read_bytes)))
+    print("--- Converting time %.5f seconds ---"
           % (time.time() - start_time))
+    print("ADC results:")
+    print(adc_results)
 
 
 if __name__ == '__main__':
